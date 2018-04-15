902fe8d210f2d7a3aa1a7a6b637158fffab801a6
Jared Lewis <leejaredlewis@gmail.com>
2018-04-15 13:11:58 -0400

    first commit

---
+++ DBFinal.py
@@ -0,0 +1,33 @@
+from flask import Flask, g, render_template
+
+from os import path
+import os
+import sqlite3
+
+print("current %s" % path.curdir)
+
+
+app = Flask(__name__)
+rel = os.path.join(app.root_path, 'sample.db')
+app.database = "C:\\Users\\leeja\\PycharmProjects\\DBFinal\\sample.db"
+
+@app.route('/')
+def index():
+    g.db = sqlite3.connect(rel)
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+
+    return render_template('index.html', posts=posts)
+
+
+
+def connect_db():
+
+    return sqlite3.connect(app.database)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
