5aec1d375062e20d15fd3bbaf023e6e0c6937808
Jared Lewis <leejaredlewis@gmail.com>
2018-04-15 21:56:24 -0400

    change to pymysql

--- DBFinal.py
+++ DBFinal.py
@@ -3,7 +3,7 @@ import sqlite3
 import pymysql
 
 conn = pymysql.connect(host="us-cdbr-iron-east-05.cleardb.net", user="b07f9bd28a1df0", password="68ccaea4",
-                       port=3306, database="heroku_bafe54ca91a5de3")
+                        database="heroku_bafe54ca91a5de3")
 cursor = conn.cursor()
 
 
